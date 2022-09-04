ad7dd567ced0d4d65f5c1678d033e5e0edb4418c
eldrogachino21 <57367901+eldrogachino21@users.noreply.github.com>
2022-09-04 02:44:50 -0500

    s

--- js/menu2.js
+++ js/menu2.js
@@ -14,7 +14,7 @@ function bodyrender(){
 
     count++;
     console.log(count)
-    document.getElementById("range").value=count;
+    document.getElementById("range").value=count.toString();
     setTimeout(function(){
         document.getElementById("Buttongenerate").disabled=false;
       }, 3000);
