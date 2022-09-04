c1f9b1a3d527d8c8530f3e39016333182887156b
eldrogachino21 <57367901+eldrogachino21@users.noreply.github.com>
2022-09-04 01:54:52 -0500

    s

--- js/menu2.js
+++ js/menu2.js
@@ -6,5 +6,9 @@ function bodyrender(){
     var seq = (Math.floor(Math.random() * 10000) + 10000).toString().substring(1);
     console.log(seq);
     document.getElementById("generatecode").innerHTML=seq;
-} 
-
+} function generate(){ 
+    document.getElementById("generatecode").innerHTML=""
+    var seq = (Math.floor(Math.random() * 10000) + 10000).toString().substring(1);
+    console.log(seq);
+    document.getElementById("generatecode").innerHTML=seq;
+}
