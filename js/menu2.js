c487583f36821fd566b39bbcc2d8a8ef23f61efe
eldrogachino21 <57367901+eldrogachino21@users.noreply.github.com>
2022-09-04 23:40:54 -0500

    s

--- js/menu2.js
+++ js/menu2.js
@@ -35,7 +35,6 @@ function generate(){
     var seq = (Math.floor(Math.random() * 10000) + 10000).toString().substring(1);
     console.log(seq);
     document.getElementById("generatecode").innerHTML=seq;
-    count=counter;
     count++;
     var contador = document.getElementById("contador")
     var input = document.getElementById("rango");
