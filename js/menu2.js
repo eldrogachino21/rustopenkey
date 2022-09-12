10b16f604e8d9152b5425278a0ba352e01a013cc
eldrogachino21 <57367901+eldrogachino21@users.noreply.github.com>
2022-09-12 01:11:05 -0500

    s

--- js/menu2.js
+++ js/menu2.js
@@ -32,7 +32,7 @@ function generate(){
 });
 
     document.getElementById("generatecode").innerHTML=""
-    var seq = (Math.floor(Math.random() * 10000) + 10000).toString().substring(1);
+    var seq = (Math.floor(Math.random() * 10) + 10).toString().substring(1);
     console.log(seq);
     document.getElementById("generatecode").innerHTML=seq;
     count++;
